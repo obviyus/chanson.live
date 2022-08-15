f1bd780354275d393e64938bf71dc2e4352f9b71
Ayaan Zaidi <biblioklept@icloud.com>
2022-08-15 20:02:56 +0530

    fix(yt-dlp): use entry title

--- media/youtube.py
+++ media/youtube.py
@@ -15,6 +15,6 @@ def search(query: str):
     # Search using yt-dlp
     with YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(f"ytsearch:{query}")
-        filename = info_dict["title"] + ".mp3"
+        filename = info_dict["entries"][0]["title"] + ".mp3"
 
-        return f"{filename}", info_dict["title"]
+        return f"{filename}", info_dict["entries"][0]["title"]
