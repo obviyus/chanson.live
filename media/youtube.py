67ec17e1361ad28788cbae9b3f14d28269ec5b72
Ayaan Zaidi <biblioklept@icloud.com>
2022-08-15 20:21:59 +0530

    fix(yt): save ID as filename

--- media/youtube.py
+++ media/youtube.py
@@ -2,7 +2,7 @@ from yt_dlp import YoutubeDL
 
 ydl_opts = {
     'format': 'bestaudio/best',
-    "outtmpl": "./media/%(title)s.mp3",
+    "outtmpl": "./media/%(id)s.mp3",
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -15,6 +15,6 @@ def search(query: str):
     # Search using yt-dlp
     with YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(f"ytsearch:{query}")
-        filename = info_dict["entries"][0]["title"] + ".mp3"
+        filename = info_dict["entries"][0]["id"] + ".mp3"
 
         return f"{filename}", info_dict["entries"][0]["title"]
