7eae5c1a5002ae29eb8eb30725d780d58496f0b2
Ayaan Zaidi <biblioklept@icloud.com>
2022-08-21 12:38:46 +0530

    feat(meta): include metadata in site

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,26 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, } from "@remix-run/react";
 import TailwindCSS from '~/styles/app.css'
+import Logo from '../public/logo.png'
+import Favicon from '../public/favicon.png'
 
 export const meta: MetaFunction = () => ({
     charset: "utf-8",
-    title: "New Remix App",
+    title: "Mach Radio",
     viewport: "width=device-width,initial-scale=1",
+    description: "A WebRTC radio station.",
+    "og:image": Logo,
+    "og:title": "Mach Radio",
+    "og:description": "A WebRTC radio station.",
 });
 
 export const links: LinksFunction = () => {
     return [
-        { rel: "stylesheet", href: TailwindCSS }
+        { rel: "stylesheet", href: TailwindCSS },
+        { rel: "icon", href: Favicon, type: "image/png" },
     ]
 }
+
 
 export default function App() {
     return (
@@ -20,6 +28,7 @@ export default function App() {
         <head>
             <Meta/>
             <Links/>
+            <title>Mach Radio</title>
         </head>
         <body>
         <Outlet/>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,9 @@
-import React, {createRef, useEffect, useState} from "react";
-import type {Consumer} from "mediasoup-client/lib/Consumer";
-import type {Transport} from "mediasoup-client/lib/Transport";
-import {Device} from "mediasoup-client";
-import {socket} from "~/utils/socket";
+import React, { createRef, useEffect, useState } from "react";
+import type { Consumer } from "mediasoup-client/lib/Consumer";
+import type { Transport } from "mediasoup-client/lib/Transport";
+import { Device } from "mediasoup-client";
+import { socket } from "~/utils/socket";
+import Logo from '../../public/logo.png'
 
 export default function Index() {
     /**
@@ -40,14 +41,14 @@ export default function Index() {
      */
     function createConsumer() {
         if (consumerTransport && device) {
-            console.log(`STARTING_CONSUMER_FOR_TRANSPORT: ${consumerTransport.id}`);
-            const {rtpCapabilities} = device;
+            console.log(`STARTING_CONSUMER_FOR_TRANSPORT: ${ consumerTransport.id }`);
+            const { rtpCapabilities } = device;
 
-            socketEmit("consume", {rtpCapabilities}).then((response: any) => {
+            socketEmit("consume", { rtpCapabilities }).then((response: any) => {
                 if (response === null) {
                     console.log("NO_PRODUCERS_AVAILABLE");
                 } else {
-                    console.log(`CONSUMING_PRODUCER: ${response['producerId']}`);
+                    console.log(`CONSUMING_PRODUCER: ${ response['producerId'] }`);
 
                     consumerTransport.consume({
                         producerId: response['producerId'],
@@ -69,7 +70,7 @@ export default function Index() {
     async function loadDevice(routerRtpCapabilities: any): Promise<Device> {
         try {
             const newDevice = new Device();
-            await newDevice.load({routerRtpCapabilities});
+            await newDevice.load({ routerRtpCapabilities });
 
             return newDevice;
         } catch (error: any) {
@@ -87,35 +88,35 @@ export default function Index() {
         });
 
         socket.on("clientCount", (message: Record<string, string>) => {
-            console.log(`CLIENT_COUNT: ${message['clientCount']}`);
+            console.log(`CLIENT_COUNT: ${ message['clientCount'] }`);
             setClientCount(parseInt(message['clientCount']));
         });
 
         socket.on('error', function (err) {
-            console.log(`SOCKET_ERROR: ${err}`);
+            console.log(`SOCKET_ERROR: ${ err }`);
         });
 
         socket.on('disconnect', function (evt) {
-            console.log(`SOCKET_DISCONNECT: ${evt}`);
+            console.log(`SOCKET_DISCONNECT: ${ evt }`);
         });
 
         socket.on("message", (message: Record<string, string>) => {
             if (message['type'] === 'welcome') {
                 if (socket.id !== message['id']) {
-                    console.log(`SOCKET_WARN ID_MISMATCH: ${message['id']} != ${socket.id}`);
+                    console.log(`SOCKET_WARN ID_MISMATCH: ${ message['id'] } != ${ socket.id }`);
                 }
 
-                console.log(`SOCKET_WELCOME: ${message['id']}`);
+                console.log(`SOCKET_WELCOME: ${ message['id'] }`);
             }
         });
 
         socket.on("producerStarted", function (message: Record<string, string>) {
-            console.log(`SOCKET_NEW_PRODUCER_STARTED: ${message['id']}`);
+            console.log(`SOCKET_NEW_PRODUCER_STARTED: ${ message['id'] }`);
             setProducerID(message['id']);
         });
 
         socket.on("producerClosed", async function (message: Record<string, string>) {
-            console.log(`SOCKET_PRODUCER_CLOSED: ${message['id']}`);
+            console.log(`SOCKET_PRODUCER_CLOSED: ${ message['id'] }`);
             setProducerID(null);
 
             if (consumer) {
@@ -128,7 +129,7 @@ export default function Index() {
         });
 
         socket.on("metadata", (message: Record<string, string>) => {
-            console.log(`SOCKET_METADATA: ${message}`);
+            console.log(`SOCKET_METADATA: ${ message }`);
             setMetadata(message);
         });
 
@@ -136,7 +137,7 @@ export default function Index() {
          * Get Router RTP Capabilities.
          */
         socketEmit('getRouterRtpCapabilities', {}).then((routerRtpCapabilities: any) => {
-            console.log(`SOCKET_GET_ROUTER_RTP_CAPABILITIES: ${routerRtpCapabilities}`);
+            console.log(`SOCKET_GET_ROUTER_RTP_CAPABILITIES: ${ routerRtpCapabilities }`);
             loadDevice(routerRtpCapabilities).then((newDevice: Device) => {
                 setDevice(newDevice);
             });
@@ -158,7 +159,7 @@ export default function Index() {
     useEffect(() => {
         socketEmit('createConsumerTransport', {}).then((response: any) => {
             if (device) {
-                console.log(`DEVICE_LOADED: ${device.loaded}`)
+                console.log(`DEVICE_LOADED: ${ device.loaded }`)
                 setConsumerTransport(device.createRecvTransport(response));
             }
         });
@@ -169,10 +170,10 @@ export default function Index() {
      */
     useEffect(() => {
         if (consumerTransport) {
-            console.log(`CONSUMER_TRANSPORT_CREATED: ${consumerTransport.id}`);
+            console.log(`CONSUMER_TRANSPORT_CREATED: ${ consumerTransport.id }`);
 
-            consumerTransport.on('connect', async ({dtlsParameters}, callback) => {
-                socketEmit('connectConsumerTransport', {dtlsParameters}).then(callback);
+            consumerTransport.on('connect', async ({ dtlsParameters }, callback) => {
+                socketEmit('connectConsumerTransport', { dtlsParameters }).then(callback);
             });
 
             consumerTransport.on('connectionstatechange', (state) => {
@@ -200,14 +201,13 @@ export default function Index() {
      */
     useEffect(() => {
         if (consumer) {
-            console.log(`CONSUMER_CREATED: ${consumer.id}`);
+            console.log(`CONSUMER_CREATED: ${ consumer.id }`);
 
             const stream = new MediaStream();
             stream.addTrack(consumer.track);
 
             if (audio.current) {
                 audio.current.srcObject = stream;
-                audio.current.volume = 0.3;
                 audio.current.play();
             }
         }
@@ -218,41 +218,51 @@ export default function Index() {
      */
     useEffect(() => {
         if (producerID !== null) {
-            console.log(`PRODUCER_CREATED: ${producerID}`);
-            console.log(`TRANSPORT: ${consumerTransport?.id}`);
-            console.log(`DEVICE: ${device?.loaded}`);
+            console.log(`PRODUCER_CREATED: ${ producerID }`);
+            console.log(`TRANSPORT: ${ consumerTransport?.id }`);
+            console.log(`DEVICE: ${ device?.loaded }`);
             createConsumer();
         }
     }, [producerID]);
 
     return (
         <div
-            className={'flex mx-auto max-w-screen-sm p-2 items-center flex-col pb-16 pt-12 min-h-screen'}>
+            className={ 'flex mx-auto max-w-screen-sm p-2 items-center flex-col pb-16 pt-12 min-h-screen' }>
+
+            <img src={ Logo }
+                 alt={ 'logo' }
+                 className={ 'w-32 h-32 mb-4 rounded-xl' }/>
+
             <h1
-                className={'text-center text-2xl font-bold'}>Mach Radio
+                className={ 'text-center text-2xl font-bold' }>Mach Radio 📻
             </h1>
 
             <p>
-                {clientCount} listener(s)
+                { clientCount } listener(s)
             </p>
 
-            <img src={metadata && metadata.cover}
-                 alt={metadata && metadata.title}
-                 className={'max-w-sm aspect-square pt-10 px-5'}
+            <img src={ metadata && metadata.cover }
+                 alt={ metadata && metadata.title }
+                 className={ 'max-w-sm aspect-square pt-10 px-5' }
             />
 
-            <h2 className={'pt-10 text-center font-bold tracking-wide'}>
-                {metadata && metadata.title} - {metadata && metadata.artist}
+            <h2 className={ 'pt-10 text-center font-bold tracking-wide' }>
+                { metadata && metadata.title }
             </h2>
 
-            <p className={'pt-2'}>
-                {metadata && metadata.album}
+            <h2 className={ 'pt-2 text-center font-semibold tracking-wide' }>
+                { metadata && metadata.artist }
+            </h2>
+
+            <p className={ 'pt-2 text-center' }>
+                { metadata && metadata.album }
             </p>
 
-            <audio ref={audio}
-                   controls={true}
+            <audio ref={ audio }
+                   controls={ true }
                    autoPlay
-                   className={'mt-10'}
+                   preload={ 'auto' }
+                   className={ 'mt-10' }
             />
         </div>
     );
