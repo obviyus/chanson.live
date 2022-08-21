5278a15fe1a09d02e2fd2df3983b903425d3f8c8
Ayaan Zaidi <biblioklept@icloud.com>
2022-08-21 12:03:54 +0530

    fix(socket): broadcast clientCount

--- server/socketHandler.ts
+++ server/socketHandler.ts
@@ -1,10 +1,10 @@
-import {io} from "./index";
-import {defaultWorker} from "./mediasoupHandler";
+import { io } from "./index";
+import { defaultWorker } from "./mediasoupHandler";
 
 export function SocketHandler() {
     io.on("connection", (socket) => {
-        console.log(`Client connected: ${socket.id}; Total clients: ${io.engine.clientsCount}`);
-        socket.emit("clientCount", {
+        console.log(`Client connected: ${ socket.id }; Total clients: ${ io.engine.clientsCount }`);
+        io.sockets.emit("clientCount", {
             clientCount: io.engine.clientsCount,
         });
 
@@ -12,8 +12,8 @@ export function SocketHandler() {
          * Handle client disconnection.
          */
         socket.on("disconnect", () => {
-                console.log(`Client disconnected: ${socket.id}; Total clients: ${io.engine.clientsCount}`);
-                socket.emit("clientCount", {
+                console.log(`Client disconnected: ${ socket.id }; Total clients: ${ io.engine.clientsCount }`);
+                io.sockets.emit("clientCount", {
                     clientCount: io.engine.clientsCount,
                 });
             }
@@ -23,17 +23,17 @@ export function SocketHandler() {
          * Log socket errors.
          */
         socket.on("error", (error) => {
-            console.error(`Error on socket: ${socket.id}: ${error}`);
+            console.error(`Error on socket: ${ socket.id }: ${ error }`);
         });
         socket.on("connect_error", (error) => {
-            console.error(`Client connection error on socket: ${socket.id}: ${error}`);
+            console.error(`Client connection error on socket: ${ socket.id }: ${ error }`);
         });
 
         /**
          * Handle getRouterRtpCapabilities request.
          */
         socket.on("getRouterRtpCapabilities", (_data, callback) => {
-            console.log(`Received getRouterRtpCapabilities request from client: ${socket.id}`);
+            console.log(`Received getRouterRtpCapabilities request from client: ${ socket.id }`);
 
             if (defaultWorker.router.rtpCapabilities) {
                 callback(defaultWorker.router.rtpCapabilities);
@@ -46,12 +46,12 @@ export function SocketHandler() {
          * Handle createConsumerTransport request.
          */
         socket.on("createConsumerTransport", async (_data, callback) => {
-            console.log(`Received createConsumerTransport request from client: ${socket.id}`);
+            console.log(`Received createConsumerTransport request from client: ${ socket.id }`);
 
             const consumerTransport = await defaultWorker.createWebRTCTransport(socket.id);
 
             consumerTransport.observer.on("close", () => {
-                console.log(`Consumer transport closed: ${consumerTransport.id}`);
+                console.log(`Consumer transport closed: ${ consumerTransport.id }`);
 
                 delete defaultWorker.transports[consumerTransport.id];
                 delete defaultWorker.consumers[consumerTransport.id];
@@ -71,15 +71,15 @@ export function SocketHandler() {
          * Handle connectConsumerTransport request.
          */
         socket.on("connectConsumerTransport", async (data, callback) => {
-            console.log(`Received connectConsumerTransport request from client: ${socket.id}`);
+            console.log(`Received connectConsumerTransport request from client: ${ socket.id }`);
 
             const consumerTransport = defaultWorker.transports[socket.id];
             if (consumerTransport) {
-                await consumerTransport.connect({dtlsParameters: data.dtlsParameters});
+                await consumerTransport.connect({ dtlsParameters: data.dtlsParameters });
                 callback({});
             } else {
                 callback(null);
-                console.error(`Consumer transport not found: ${socket.id}`);
+                console.error(`Consumer transport not found: ${ socket.id }`);
             }
         });
 
@@ -96,7 +96,7 @@ export function SocketHandler() {
 
             const consumerTransport = defaultWorker.transports[socket.id];
             if (!consumerTransport) {
-                console.error(`Consumer transport not found: ${socket.id}`);
+                console.error(`Consumer transport not found: ${ socket.id }`);
                 return;
             }
 
