367ed1e0397fa6f98a6f2a962d435ccacbdf8b6b
Ayaan Zaidi <zaidi@uplause.io>
2022-12-16 18:24:44 +0530

    chore(config): bump deps + fmt config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: ['./app/utils/player.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
