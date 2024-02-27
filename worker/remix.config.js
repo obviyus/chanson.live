06bc3b9283d8a96e712144d6c49d3a566e351cb6
Ayaan Zaidi <zaidi@uplause.io>
2024-02-27 18:16:45 +0530

    build(tailwind): handle tailwind build in remix

--- worker/remix.config.js
+++ worker/remix.config.js
@@ -1,10 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
-  tailwind: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
-  browserNodeBuiltinsPolyfill: { modules: { events: true } },
+	ignoredRouteFiles: ["**/.*"],
+	tailwind: true,
+	postcss: true,
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// publicPath: "/build/",
+	// serverBuildPath: "build/index.js",
+	browserNodeBuiltinsPolyfill: { modules: { events: true } },
 };
